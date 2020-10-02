a7751b448d099fabac34b360f1314982b5c87aed
DiegoOno <diego.ono1505@gmail.com>
2020-10-02 10:35:47 -0300

    Finished home page

--- src/components/NumberSetter/index.tsx
+++ src/components/NumberSetter/index.tsx
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {FaArrowUp, FaArrowDown} from 'react-icons/fa';
 
 import './styles.css';
 
-const NumberSetter = () => {
-  const buttonColor = '#333853'
+interface NumberSetterProps {
+  title: String,
+  defaultValue: number
+};
+
+const NumberSetter: React.FC<NumberSetterProps> = ({ title, defaultValue }) => {
+  const buttonColor = '#FFFFFF'
+
+  const [inputValue, setInputValue] = useState(defaultValue);
+
+  const increment = () => {
+    if (inputValue < 60) {
+      setInputValue(inputValue + 1);
+    } else {
+      setInputValue(0);
+    }
+  }
+
+  const decrement = () => {
+    if (inputValue > 0) {
+      setInputValue(inputValue - 1);
+    } else {
+      setInputValue(60);
+    }
+  }
 
   return (
     <div className='wrapper'>
       <div className='data-field'>
         <div className="button-wrapper">
-          <button>
+          <button onClick={increment}>
             <FaArrowUp size={20} color={buttonColor}/>
           </button>
-          <button>
+          <button onClick={decrement}>
             <FaArrowDown size={20} color={buttonColor}/>
           </button>
         </div>
         <div className='input-field'>
-          <input type='number' max='59' min='00'/>
+          <input 
+            type='number' 
+            max='59' 
+            min='00' 
+            value={inputValue}
+            disabled
+          />
         </div>
       </div>
-      type
+      <span>
+        {title}
+      </span>
     </div>
   );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import NumberSetter from '../../components/NumberSetter';
 import './styles.css';
@@ -8,11 +9,13 @@ const Home = () => {
     <div className='container'>
       <h1>Pomodoro</h1>
       <div className='pomodoro-config-field'>
-        <NumberSetter />
-        <NumberSetter />
-        <NumberSetter />
+        <NumberSetter title='Trabalho' defaultValue={25}/>
+        <NumberSetter title='Pausa' defaultValue={5}/>
+        <NumberSetter title='Sessões' defaultValue={3}/>
       </div>
-      <button className='confirm-button'><h1>Continuar</h1></button>
+      <Link to='/timer'>
+        <button className='confirm-button'><h1>Continuar</h1></button>
+      </Link>
     </div>
   );
 };
